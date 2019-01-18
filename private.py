e1a626c83ea0bcf2169b41bd95eed8e22bcde85e
Martin Ruskov <martin@unriddle.it>
2019-01-18 07:59:49 +0100

    added OpenCorporates API key placeholder

--- private.py
+++ private.py
@@ -1,1 +1,10 @@
+"""This file represents your personalisation.
+In order to use the full potential of the platform,
+please register to the following services to be able to use their data.
+All of these offer some form of free subscription plan.
+The data that you will fetch from these services will be cached in the data/ directory of your project.
+Do not distribute this data. It is for your personal use only."""
+
+
 bankcodes_api_key = "" # Get your key from https://bank.codes/users/register/
+oc_api_key = "" # from https://opencorporates.com/api_accounts/new
